d775dfec695a1ebf6faada3373ce254f54d84017
RahmanianEssa <rahmanianessa1@gmail.com>
2020-03-28 19:44:16 +0700

    button laptop

--- app/src/main/java/com/nian/project_uts/LaptopActivity.java
+++ app/src/main/java/com/nian/project_uts/LaptopActivity.java
@@ -2,6 +2,7 @@ package com.nian.project_uts;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -18,21 +19,20 @@ public class LaptopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laptop);
-        RecyclerView teamsView = findViewById(R.id.rv_laptop);
+        RecyclerView laptopView = findViewById(R.id.rv_laptop);
 
-        List<Computer> teams = new ArrayList<>();
-        teams.add(new Computer(R.drawable.acer,"Acer E5-573 Laptop","Processor : intel core i3", "Rp7.700.000" ));
-        teams.add(new Computer(R.drawable.hp, "HP Spectre x360 - 13-ap0052tu", "Processor : intel core i7" , "Rp24.009.020"  ));
-        teams.add(new Computer(R.drawable.asus,"ASUS VivoBook E402BA","Processor: AMD A4-9125" ,"Rp3.649.000"));
-        teams.add(new Computer(R.drawable.tosibah,"TOSHIBA satellite C40-A106 ", " processor : Intel Core i3-2348M", "Rp 6.045.000"));
-        teams.add(new Computer(R.drawable.acer2,"ACER Swift 3 SF314-54 ","Processor: Intel Core i7-8550U","Rp 13.831.900"));
-        teams.add(new Computer(R.drawable.dell, "DELL Vostro 14-3480","Processor: Intel Core i5-8265U","Rp 11.119.000"));
+        List<Computer> laptop = new ArrayList<>();
+        laptop.add(new Computer(R.drawable.acer, "Acer"));
+        laptop.add(new Computer(R.drawable.hp, "Hp"));
+        laptop.add(new Computer(R.drawable.asus, "Asus"));
+        laptop.add(new Computer(R.drawable.tosibah, "Toshiba"));
+        laptop.add(new Computer(R.drawable.dell, "Dell"));
 
-        LaptopAdapter adapter = new LaptopAdapter(this,teams);
-        teamsView.setAdapter(adapter);
+        LaptopAdapter adapter = new LaptopAdapter(this,laptop);
+        laptopView.setAdapter(adapter);
 
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
-        teamsView.setLayoutManager(layoutManager);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        laptopView.setLayoutManager(layoutManager);
     }
     }
 
--- app/src/main/java/com/nian/project_uts/adapters/LaptopAdapter.java
+++ app/src/main/java/com/nian/project_uts/adapters/LaptopAdapter.java
@@ -10,19 +10,20 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.squareup.picasso.Picasso;
 
 import com.bumptech.glide.Glide;
+import com.nian.project_uts.LaptopActivity;
 import com.nian.project_uts.R;
 import com.nian.project_uts.model.Computer;
-import com.squareup.picasso.Picasso;
+
 
 import org.w3c.dom.Text;
 
 import java.util.List;
 
 public class LaptopAdapter extends RecyclerView.Adapter<LaptopAdapter.ViewHolder> {
-    private static final int LOGO_GREY =2;
-    private static final int LOGO_WHITE=1;
+
     private Context context;
     private List<Computer> items;
 
@@ -34,54 +35,36 @@ public class LaptopAdapter extends RecyclerView.Adapter<LaptopAdapter.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view=null;
-        if (LOGO_GREY==viewType){
-            view= LayoutInflater.from(context)
-                    .inflate(R.layout.item_laptop, parent, false);
-            view.setBackgroundColor(Color.rgb(227, 221, 200));
-        } else if (viewType==LOGO_WHITE){
-            view=LayoutInflater.from(context)
-                    .inflate(R.layout.item_laptop, parent , false);
-        }
-        return new LaptopAdapter.ViewHolder(view);
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.item_laptop, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull LaptopAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
     Computer item = items.get(position);
-        Glide.with(context).load(item.getGambar()).into(holder.gambarImage);
         holder.namaText.setText(item.getNama());
-        holder.deskribText.setText(item.getDeskripsi());
-        holder.hargaText.setText(item.getHarga());
+        Glide.with(context).load(item.getGambar()).into(holder.gambarImage);
     }
 
-    @Override
-
-    public long getItemId(int position) {
-        return (position % 4 == 3 || position % 4 == 0) ? LOGO_GREY : LOGO_WHITE;
-    }
 
     @Override
     public int getItemCount() {
+
         return (items != null) ? items.size() : 0;
     }
 
-    @Override
-    public int getItemViewType(int position) {
-        return (position % 4 == 3 || position % 4 == 0) ? LOGO_GREY : LOGO_WHITE;
-    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView gambarImage;
         TextView namaText;
-        TextView deskribText;
-        TextView hargaText;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
          gambarImage = itemView.findViewById(R.id.logo_laptop);
          namaText=itemView.findViewById(R.id.text_name);
-         deskribText=itemView.findViewById(R.id.text_deskripsi);
-         hargaText=itemView.findViewById(R.id.harga);
+
         }
     }
 }
